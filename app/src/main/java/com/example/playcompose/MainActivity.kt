c2b4ee75079e8effac09c1567bf13cc43af2932a
wentaoKing <jinwentao@cvte.com>
2022-02-13 17:41:58 +0800

    compose的重组和Remember

--- app/src/main/java/com/example/playcompose/MainActivity.kt
+++ app/src/main/java/com/example/playcompose/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.example.playcompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.lifecycle.lifecycleScope
 import com.example.playcompose.ui.getNormalView
 import kotlinx.coroutines.delay
@@ -24,17 +21,33 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // mutableStateOf具备订阅功能，内部会去取值
+//        var name by mutableStateOf("compose")
 
         // 使用by委托需要手动import androidx.compose.runtime.getValue/setValue，目前不支持自动导入
-        var name by mutableStateOf("composable")
         setContent {
 //            getNormalView()
-            // mutableStateOf具备订阅功能，内部会去取值
-            Text(name)
+//            Text(name)
+            //  var name by mutableStateOf("compose")
+            UseRemember()
         }
+
+    }
+
+    /**
+     * 重组过程：输入更改时再次调用可组合函数的过程（Recompose过程）
+     * remember可以避免二次执行时，变量的重新初始化，它会去拿之前缓存的值
+     */
+    @Composable
+    fun UseRemember() {
+        var name by remember {
+            mutableStateOf("compose")
+        }
+        Text("name is $name")
+        Log.d(TAG, "UseRemember: ")
         lifecycleScope.launch {
             delay(3000)
-            name = "composable 3000"
+            name = "Recompose"
         }
     }
 
